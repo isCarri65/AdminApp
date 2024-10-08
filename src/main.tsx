062d3c5806cb996f5637cca8640c0a67bbc8a8af
Ismael Carrillo <iscarrillo65@gmail.com>
2024-10-08 19:13:55 -0300

    Subiendo proyecto base, commit 1

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './redux/Store/Store.ts'
+import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>,
+)
