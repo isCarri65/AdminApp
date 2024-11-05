a3a6a8ea299f5b63a5744cb319b36b263d4a6a11
Matias Jofré <53499784+Matijofre3@users.noreply.github.com>
2024-11-05 18:43:07 -0300

    tablaProd

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../components/screens/HomeSecundario/HomeSecundario";
 import { HomePrincipal } from "../components/screens/HomePrincipal/HomePrincipal";
+import ProductTable from "../components/screens/ProductTable/ProductTable";
 
 export const AppRouter = () => {
   return (
@@ -12,6 +13,7 @@ export const AppRouter = () => {
         {/* Rutas */}
         <Route path="/" element={<HomePrincipal />} />
         <Route path="/HomeSecundario/:id" element={<Home />} />
+        <Route path="/HomeSecundario/sucursal/:id" element={<ProductTable />} />
       </Routes>
     </>
   );
